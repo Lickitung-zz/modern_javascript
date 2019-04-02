99ed632d4810fb327a44d0728855e899c538fda0
SIeep <brandon_n_evans@live.com>
2019-04-01 20:41:15 -0500

    add npm, webpack and Babel

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+var moment = require('moment');
+// var math = require('mathjs');
+
+var name = "Bob", time = "today";
+console.log(`Hello ${name}, how are you ${time}?`);
+
+
 console.log("hello");
 console.log("goodbye");
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+// console.log(math.round(math.e, 3));
